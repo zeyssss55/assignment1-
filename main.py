9b903a04cfa18e1eafa0865ac7b5eedf5934184b
zeyssss55 <161566570+zeyssss55@users.noreply.github.com>
2024-02-28 14:56:08 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+name = "Fahri"
+age = "17"
+live = "cibatu"
+hobby = "reading"
+major = "informatic engineering"
+gender = "Male"
+
+
+print("my name is =",name)
+print("my age is =",age)
+print("i live in =",live)
+print("my hobby is =",hobby)
+print("my major is =",major)
+print("my gender is =", gender)
+
